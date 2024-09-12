363bbf669b88cc5c884287b4e4d3e57206ede158
anatolii_bezkrovnyi <bezkrovnyi1@gmail.com>
2024-09-13 00:48:39 +0300

    main structure

--- src/api/movie-search-api.js
+++ src/api/movie-search-api.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://dummyjson.com';
-export const fetchPosts = async () => { 
-   const {data} = await axios.get('/posts');
-   return data.posts;
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+const options = {
+  headers: {
+	// Замість api_read_access_token вставте свій токен
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjA4MWJmMDBkMDdhYzhmMDRhNWUyZjMzZmZmOTUxNiIsIm5iZiI6MTcyNjE3MDA0OS40MzgzNCwic3ViIjoiNjZlMzNjMzJmNDY3YzJhZDYyZjkyNmU2Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.8o5Yev22ShnXRt4e8Vpy5ZYYzPxzdpHp0Y-e7DFSg58'
+  }
+};
+export const fetchTrendingMovies = async () => { 
+   const url = 'trending/movie/day?language=en-US';
+   const response = await axios.get(url, options);
+   return response.data;
 }
  
 export const fetchPost = async (id) => { 
   const {data} = await axios.get(`/post/${id}`);
   return data;
- }
+}
+ 
+
+
